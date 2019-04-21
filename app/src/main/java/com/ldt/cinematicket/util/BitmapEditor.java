e78393e625e113b0758ea2f3fad18e3570edd30b
Daiyr <daiyrbek1705@gmail.com>
2019-04-21 22:28:54 +0600

    Update BitmapEditor.java

--- app/src/main/java/com/ldt/cinematicket/util/BitmapEditor.java
+++ app/src/main/java/com/ldt/cinematicket/util/BitmapEditor.java
@@ -600,3 +600,336 @@ public final class BitmapEditor {
         canvas.drawBitmap(bitmap, rect, rect, paint);
         return output;
     }
+    public static Bitmap getResizedBitmap(Bitmap image, int newHeight, int newWidth) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // create A matrix for the manipulation
+        Matrix matrix = new Matrix();
+        // resize the bit map
+        matrix.postScale(scaleWidth, scaleHeight);
+        // recreate the new Bitmap
+        Bitmap resizedBitmap = Bitmap.createBitmap(image, 0, 0, width, height,
+                matrix, false);
+        return resizedBitmap;
+    }
+    public static boolean TrueIfBitmapBigger(Bitmap bitmap, int size)
+    {
+        int sizeBitmap= (bitmap.getHeight()>bitmap.getWidth()) ? bitmap.getHeight() : bitmap.getWidth();
+        if(sizeBitmap>size) return  true;
+        return false;
+    }
+    public static Bitmap GetRoundedBitmapWithBlurShadow(Bitmap original, int paddingTop, int paddingBottom, int paddingLeft, int paddingRight) {
+        int original_width= original.getWidth();
+        int orginal_height= original.getHeight();
+        int bitmap_width= original_width+paddingLeft+paddingRight;
+        int bitmap_height= orginal_height+ paddingTop+paddingBottom;
+        Bitmap bitmap = Bitmap.createBitmap(bitmap_width,bitmap_height,Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        //paint.setAlpha(60);
+        //   canvas.drawRect(0,0,bitmap_width,bitmap_height,paint);
+        paint.setAntiAlias(true);
+        canvas.drawBitmap(original,paddingLeft,paddingTop,paint);
+        Bitmap blurred_bitmap= getBlurredWithGoodPerformance(bitmap,1,6,4);
+        canvas.setBitmap(null);
+        bitmap.recycle();
+        return blurred_bitmap;
+    }
+
+    //                                                                            Activity.
+    //                                                                                |            Original bitmap.
+    //                                                                                 |                      |              To make the blur background, the original must to padding.
+    //                                                                                  |                      |                          |                |                            |                       |
+    //                                                                                   V                     V                         V              V                           V                      V
+    public static Bitmap GetRoundedBitmapWithBlurShadow(Context context, Bitmap original, int paddingTop, int paddingBottom, int paddingLeft, int paddingRight,
+                                                        int TopBack     // this value makes the overview bitmap is higher or  belower the background.
+            , int alphaBlurBackground // this is the alpha of the background Bitmap, you need A number between 0 -> 255, the value recommended is 180.
+            , int valueBlurBackground // this is the value used to blur the background Bitmap, the recommended one is 12.
+            , int  valueSaturationBlurBackground // this is the value used to background Bitmap more colorful, if valueBlur is 12, the valudeSaturation should be 2.
+    ) {
+        int original_width= original.getWidth();
+        int orginal_height= original.getHeight();
+        int bitmap_width= original_width+paddingLeft+paddingRight;
+        int bitmap_height= orginal_height+ paddingTop+paddingBottom;
+        Bitmap bitmap = Bitmap.createBitmap(bitmap_width,bitmap_height,Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setAntiAlias(true);
+        canvas.drawBitmap(original,paddingLeft,paddingTop,paint);
+        Bitmap blurred_bitmap= getBlurredWithGoodPerformance(context, bitmap,1,valueBlurBackground,valueSaturationBlurBackground);
+        //   Bitmap blurred_bitmap= getBlurredWithGoodPerformance(context, bitmap,1,15,3);
+        Bitmap end_bitmap = Bitmap.createBitmap(bitmap_width,bitmap_height, Bitmap.Config.ARGB_8888);
+        canvas.setBitmap(end_bitmap);
+        paint.setAlpha(alphaBlurBackground);
+
+        canvas.drawBitmap(blurred_bitmap,new Rect(0,0,blurred_bitmap.getWidth(),blurred_bitmap.getHeight()),new Rect(0,0,bitmap_width,bitmap_height),paint);
+        paint.setAlpha(255);
+
+        canvas.drawBitmap(bitmap,0,TopBack,paint); // drawVisualWave cái lớn
+        canvas.setBitmap(null);
+        blurred_bitmap.recycle();
+        bitmap.recycle();
+        return end_bitmap;
+    }
+    public static void setBitmapforImageView(ImageView imv,Bitmap apply)
+    {
+        Bitmap old = ((BitmapDrawable)imv.getDrawable()).getBitmap();
+        imv.setImageBitmap(apply);
+        if(old!=null)
+            old.recycle();
+    }
+    public static Bitmap getBlurredWithGoodPerformance(Bitmap bitmap,int scale,int radius,int saturation)
+    {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap bitmap1=getResizedBitmap(bitmap,50,50);
+        Bitmap updateSatBitmap = updateSat(bitmap1,saturation);
+        Bitmap blurredBitmap= FastBlurSupportAlpha(updateSatBitmap,scale,radius);
+
+        updateSatBitmap.recycle();
+        bitmap1.recycle();
+        return blurredBitmap;
+    }
+    static public Path RoundedRect(float left, float top, float right, float bottom, float rx, float ry, boolean conformToOriginalPost) {
+        Path path = new Path();
+        if (rx < 0) rx = 0;
+        if (ry < 0) ry = 0;
+        float width = right - left;
+        float height = bottom - top;
+        if (rx > width/2) rx = width/2;
+        if (ry > height/2) ry = height/2;
+        float widthMinusCorners = (width - (2 * rx)); // do dai phan "thang" cua chieu rong
+        float heightMinusCorners = (height - (2 * ry)); // do dai phan "thang" cua chieu dai
+
+        path.moveTo(right, top + ry);  // bat dau tu  day
+        path.rQuadTo(0, -ry, -rx, -ry);//y-right corner
+        path.rLineTo(-widthMinusCorners, 0);
+        path.rQuadTo(-rx, 0, -rx, ry); //y-x corner
+        path.rLineTo(0, heightMinusCorners);
+
+        if (conformToOriginalPost) {
+            path.rLineTo(0, ry);
+            path.rLineTo(width, 0);
+            path.rLineTo(0, -ry);
+        }
+        else {
+
+            path.rQuadTo(0, ry, rx, ry);//bottom-x corner
+            path.rLineTo(widthMinusCorners, 0);
+            path.rQuadTo(rx, 0, rx, -ry); //bottom-right corner
+        }
+
+        path.rLineTo(0, -heightMinusCorners);
+
+        path.close();//Given close, last lineto can be removed.
+
+        return path;
+    }
+    public static int mixTwoColors( int color1, int color2, float amount )
+    {
+        final byte ALPHA_CHANNEL = 24;
+        final byte RED_CHANNEL   = 16;
+        final byte GREEN_CHANNEL =  8;
+        final byte BLUE_CHANNEL  =  0;
+
+        final float inverseAmount = 1.0f - amount;
+
+        int a = ((int)(((float)(color1 >> ALPHA_CHANNEL & 0xff )*amount) +
+                ((float)(color2 >> ALPHA_CHANNEL & 0xff )*inverseAmount))) & 0xff;
+        int r = ((int)(((float)(color1 >> RED_CHANNEL & 0xff )*amount) +
+                ((float)(color2 >> RED_CHANNEL & 0xff )*inverseAmount))) & 0xff;
+        int g = ((int)(((float)(color1 >> GREEN_CHANNEL & 0xff )*amount) +
+                ((float)(color2 >> GREEN_CHANNEL & 0xff )*inverseAmount))) & 0xff;
+        int b = ((int)(((float)(color1 & 0xff )*amount) +
+                ((float)(color2 & 0xff )*inverseAmount))) & 0xff;
+
+        return a << ALPHA_CHANNEL | r << RED_CHANNEL | g << GREEN_CHANNEL | b << BLUE_CHANNEL;
+    }
+    public static Bitmap getBlurredWithGoodPerformance(Context context,Bitmap bitmap,int scale,int radius,float saturation)
+    {
+        Bitmap bitmap1=getResizedBitmap(bitmap,150,150);
+        Bitmap updateSatBimap = updateSat(bitmap1,saturation);
+        Bitmap blurredBitmap= BlurBitmapWithRenderScript(context,updateSatBimap,radius);
+        updateSatBimap.recycle();
+        bitmap1.recycle();
+        return blurredBitmap;
+    }
+
+    public static Bitmap getBlurredBimapWithRenderScript(Context context, Bitmap bitmapOriginal,float radius)
+    {
+        //define this only once if blurring multiple times
+        RenderScript rs = RenderScript.create(context);
+
+//this will blur the bitmapOriginal with A radius of 8 and save it in bitmapOriginal
+        final Allocation input = Allocation.createFromBitmap(rs, bitmapOriginal); //use this constructor for best performance, because it uses USAGE_SHARED mode which reuses memory
+        final Allocation output = Allocation.createTyped(rs, input.getType());
+        final ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        script.setRadius(radius);
+        script.setInput(input);
+        script.forEach(output);
+        output.copyTo(bitmapOriginal);
+        return bitmapOriginal;
+    }
+    public static Bitmap BlurBitmapWithRenderScript(Context context, Bitmap bitmap, float radius){
+        //Let's create an empty bitmap with the same mID of the bitmap we want to blur
+        Bitmap outBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Instantiate A new Renderscript
+        RenderScript rs = RenderScript.create(context);
+
+        //Create an Intrinsic Blur Script using the Renderscript
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+
+        //Create the Allocations (in/out) with the Renderscript and the in/out bitmaps
+        Allocation allIn = Allocation.createFromBitmap(rs, bitmap);
+        Allocation allOut = Allocation.createFromBitmap(rs, outBitmap);
+
+        //Set the radius of the blur
+        blurScript.setRadius(radius);
+
+        //Perform the Renderscript
+        blurScript.setInput(allIn);
+        blurScript.forEach(allOut);
+
+        //Copy the final bitmap created by the out Allocation to the outBitmap
+        allOut.copyTo(outBitmap);
+
+        //recycle the original bitmap
+
+        //After finishing everything, we destroy the Renderscript.
+        rs.destroy();
+
+        return outBitmap;
+
+
+    }
+
+
+    public static Drawable covertBitmapToDrawable(Context context, Bitmap bitmap) {
+        Drawable d = new BitmapDrawable(context.getResources(), bitmap);
+        return d;
+    }
+
+    public static Bitmap convertDrawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+    public static Bitmap changeBitmapColor(Bitmap sourceBitmap, int color)
+    {
+        Bitmap resultBitmap = sourceBitmap.copy(sourceBitmap.getConfig(),true);
+        Paint paint = new Paint();
+        ColorFilter filter =  new PorterDuffColorFilter(color,PorterDuff.Mode.SRC_IN);
+        paint.setColorFilter(filter);
+        Canvas canvas = new Canvas(resultBitmap);
+        canvas.drawBitmap(resultBitmap, 0, 0, paint);
+        return resultBitmap;
+    }
+
+    /**
+     *
+     * @param mode
+     * @return
+     *  0 : CLEAR
+     *  <br> 1 : SRC
+     *  <br> 2 : DST
+     *  <br> 3 : SRC_OVER
+     *  <br> 4 : DST_OVER
+     *  <br> 5 : SRC_IN
+     *  <br> 6 : DST_IN
+     *  <br> 7 : SRC_OUT
+     *  <br> 8 : DST_OUT
+     *  <br> 9 : SRC_ATOP
+     *  <br>10 : DST_ATOP
+     *  <br>11 : XOR
+     *  <br>12 : ADD
+     *  <br>13 : MULTIPLY
+     *  <br>14 : SCREEN
+     *  <br>15 : OVERLAY
+     *  <br>16 : DARKEN
+     *  <br>17 : LIGHTEN
+     */
+    public static PorterDuff.Mode getPorterMode(int mode)
+    {
+        switch (mode) {
+            default:
+            case  0: return PorterDuff.Mode.CLEAR;
+            case  1: return PorterDuff.Mode.SRC;
+            case  2: return PorterDuff.Mode.DST;
+            case  3: return PorterDuff.Mode.SRC_OVER;
+            case  4: return PorterDuff.Mode.DST_OVER;
+            case  5: return PorterDuff.Mode.SRC_IN;
+            case  6: return PorterDuff.Mode.DST_IN;
+            case  7: return PorterDuff.Mode.SRC_OUT;
+            case  8: return PorterDuff.Mode.DST_OUT;
+            case  9: return PorterDuff.Mode.SRC_ATOP;
+            case 10: return PorterDuff.Mode.DST_ATOP;
+            case 11: return PorterDuff.Mode.XOR;
+            case 16: return PorterDuff.Mode.DARKEN;
+            case 17: return PorterDuff.Mode.LIGHTEN;
+            case 13: return PorterDuff.Mode.MULTIPLY;
+            case 14: return PorterDuff.Mode.SCREEN;
+            case 12: return PorterDuff.Mode.ADD;
+            case 15: return PorterDuff.Mode.OVERLAY;
+        }
+    }
+    public static void applyNewColor4Bitmap(Context context,int[] idBitmaps,ImageView[] imageViews,int color,float alpha)
+    {
+        android.content.res.Resources resource =  context.getResources();
+        int size= idBitmaps.length;
+        Bitmap usingBitmap,resultBitmap;
+        for(int i=0;i<size;i++)
+        {
+            usingBitmap = BitmapFactory.decodeResource(resource,idBitmaps[i]);
+            resultBitmap = changeBitmapColor(usingBitmap,color);
+            imageViews[i].setImageBitmap(resultBitmap);
+            imageViews[i].setAlpha(alpha);
+        }
+    }
+    public static void applyNewColor4Bitmap(Context context,int idBitmap,ImageView applyView,int color,float alpha)
+    {
+
+        android.content.res.Resources resource =  context.getResources();
+        Bitmap usingBitmap = BitmapFactory.decodeResource(resource,idBitmap);
+        Bitmap resultBitmap = changeBitmapColor(usingBitmap,color);
+        applyView.setImageBitmap(resultBitmap);
+        applyView.setAlpha(alpha);
+
+    }
+    public static Bitmap getBitmapFromView(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bitmap);
+        view.layout(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+        view.draw(c);
+        return bitmap;
+    }
+    public static Bitmap getBitmapFromView(View view,int left,int top,int right,int bottom) {
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bitmap);
+        view.layout(left,top,right,bottom);
+        view.draw(c);
+        return bitmap;
+    }
+    public static Bitmap getBackgroundBitmapAViewWithParent(View childView,View parentView)
+    {
+        int[] pos_child  = new int[2];
+        childView.getLocationOnScreen(pos_child);
+        return getBitmapFromView(parentView,pos_child[0],pos_child[1],parentView.getRight(),parentView.getBottom());
+    }
+    public static Bitmap getBackgroundBlurAViewWithParent(Activity activity,View childView,View parentView)
+    {
+        Bitmap b1= getBackgroundBitmapAViewWithParent(childView, parentView);
+        Bitmap b2 = getBlurredWithGoodPerformance(activity,b1,1,8,2);
+        b1.recycle();
+        return b2;
+    }
+}
