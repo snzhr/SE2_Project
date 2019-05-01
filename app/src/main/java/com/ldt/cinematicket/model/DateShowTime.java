b7a76a8ec0d137a4a03b0cf9b03f5cdf119af609
saparbek05 <31755204+saparbek05@users.noreply.github.com>
2019-05-01 23:26:55 +0600

    Update DateShowTime.java

--- app/src/main/java/com/ldt/cinematicket/model/DateShowTime.java
+++ app/src/main/java/com/ldt/cinematicket/model/DateShowTime.java
@@ -4,11 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
 
-/**
- * Showtime contains an array of projection dates,
- * each day has a projection date, and a projection time
- * each projection time contains a projection room, a ticket price and an array of boolean seats showing whether the seat is empty or not, the default seat number is 25
- */
 
 public class DateShowTime {
     @Override
